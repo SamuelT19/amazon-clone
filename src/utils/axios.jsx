23d97c0ae3773db003a3dfc0f9fe49aa4f6d7d7f
SamuelT19 <139996430+SamuelT19@users.noreply.github.com>
2024-04-22 17:16:51 +0300

    Update axios.jsx

--- src/utils/axios.jsx
+++ src/utils/axios.jsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const base = axios.create({
-  baseURL: `https://fakestoreapi.com`,
+  baseURL: `https://fakestoreapi.com/`,
 });
 
 export const functionAxios = axios.create({
